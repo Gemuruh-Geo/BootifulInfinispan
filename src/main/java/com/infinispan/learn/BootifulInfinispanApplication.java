d6a312ef1519369f241a904f26c0d9d9a25de322
GemuruhGeo <gemuruh.geo.pratama@gmail.com>
2017-12-14 10:40:22 +0700

    Firt commit, basic stracture springboot without embedded tomcat

---
+++ src/main/java/com/infinispan/learn/BootifulInfinispanApplication.java
@@ -0,0 +1,12 @@
+package com.infinispan.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulInfinispanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifulInfinispanApplication.class, args);
+	}
+}
