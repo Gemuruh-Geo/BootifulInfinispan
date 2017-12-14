990fe76c2eb02ab52a6941c6d9d2555763034a2c
GemuruhGeo <gemuruh.geo.pratama@gmail.com>
2017-12-14 10:48:45 +0700

    Maven configuration

--- src/main/java/com/infinispan/learn/BootifulInfinispanApplication.java
+++ src/main/java/com/infinispan/learn/BootifulInfinispanApplication.java
@@ -1,12 +1,18 @@
 package com.infinispan.learn;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootifulInfinispanApplication {
+public class BootifulInfinispanApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BootifulInfinispanApplication.class, args);
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		System.out.println("Hello World");
 	}
 }
