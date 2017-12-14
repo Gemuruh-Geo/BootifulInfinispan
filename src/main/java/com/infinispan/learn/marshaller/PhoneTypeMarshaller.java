da25fcc05b0fd54c00a837379389f0332f7c34e4
GemuruhGeo <gemuruh.geo.pratama@gmail.com>
2017-12-14 14:22:44 +0700

    Phone Type Marshaller

---
+++ src/main/java/com/infinispan/learn/marshaller/PhoneTypeMarshaller.java
@@ -0,0 +1,36 @@
+package com.infinispan.learn.marshaller;
+
+import com.infinispan.learn.entity.PhoneType;
+import org.infinispan.protostream.EnumMarshaller;
+
+public class PhoneTypeMarshaller implements EnumMarshaller<PhoneType> {
+
+    @Override
+    public Class<? extends PhoneType> getJavaClass() {
+        return PhoneType.class;
+    }
+
+    @Override
+    public String getTypeName() {
+        return "proto.Person.PhoneType";
+    }
+
+    @Override
+    public PhoneType decode(int i) {
+
+        return null;
+    }
+
+    @Override
+    public int encode(PhoneType phoneType) throws IllegalArgumentException {
+        switch (phoneType){
+            case HOME:
+                return 0;
+            case WORK:
+                return 1;
+            case MOBILE:
+                return 2;
+        }
+        throw new IllegalArgumentException("Unexpected PhoneType value : " + phoneType);
+    }
+}
